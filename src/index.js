f0b2ec6617e1cc0d5e751cdb35d8b8fc76f3213a
deepakchoursiya <chourasiadeepak2002@gmail.com>
2025-03-03 16:29:28 +0530

    Deepak Add. Added ability to view persons by city or state using filter() and map()

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const addressBook = new AddressBook();
 
 try {
     const contact1 = new Contact(
-        "Deepak", "Choursiya", "Bhopal", "India", "In", "100001", "9876543210", "chourasiadeepak2002@gmail.com"
+        "Deepak", "Chourasia", "Bhopal", "India", "In", "100001", "9876543210", "chourasiadeepak2002@gmail.com"
     );
     console.log(addressBook.addContact(contact1)); 
 
@@ -26,7 +26,10 @@ try {
     console.log(addressBook.addContact(duplicateContact));  
 
     console.log("Contacts in India:");
-    console.log(addressBook.searchByCityOrState("India"));
+    console.log(addressBook.searchByCityOrState("India")); 
+
+    console.log("Viewing Persons by City or State:");
+    console.log(addressBook.viewPersonsByCityOrState());
 
 } catch (error) {
     console.error("Error:", error.message);
