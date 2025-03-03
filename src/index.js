03066c28f58a479b98209ade3ddb838e562f71d2
deepakchoursiya <chourasiadeepak2002@gmail.com>
2025-03-03 16:04:46 +0530

    Deepak Add. Added ability to count contacts using reduce()

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ const addressBook = new AddressBook();
 
 try {
     const contact1 = new Contact(
-        "Deepak", "Choursiya","Bhopal", "India", "In", "100001", "9876543210", "chourasiadeepak2002@gmail.com"
+        "Deepak", "Choursiya", "Bhopal", "India", "In", "100001", "9876543210", "chourasiadeepak2002@gmail.com"
     );
     console.log(addressBook.addContact(contact1)); 
 
     const contact2 = new Contact(
-        "Rakesh", "Soni", "Bhopal", "India","In","1000000","9197979988","rakesh395@gmail.com"
+        "Rakesh", "Soni", "Bhopal", "India","In","1000000","9197979988","rakeshsoni395@gmail.com"
     )
     console.log(addressBook.addContact(contact2)); 
     console.log("All Contacts Before editing", addressBook.getAllContacts());
@@ -18,6 +18,7 @@ try {
     console.log("All Contacts After editing", addressBook.getAllContacts());
     console.log(addressBook.deleteContact("Rakesh"));
     console.log("All Contacts After Deleting:", addressBook.getAllContacts());
+    console.log("Total Contacts:", addressBook.countContacts());
 } catch (error) {
     console.error("Error:", error.message);
 }
