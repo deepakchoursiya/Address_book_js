67fc8d41eb39835225e382978e7bf9e2d18f8b90
deepakchoursiya <chourasiadeepak2002@gmail.com>
2025-03-03 15:59:58 +0530

    Deepak Add.  Added ability to find and delete contact by name

--- src/index.js
+++ src/index.js
@@ -5,17 +5,19 @@ const addressBook = new AddressBook();
 
 try {
     const contact1 = new Contact(
-        "Deepak", "Choursiya", "Bhopal", "India", "In", "100001", "9876543210", "chourasiadeepak2002@gmail.com"
+        "Deepak", "Choursiya","Bhopal", "India", "In", "100001", "9876543210", "chourasiadeepak2002@gmail.com"
     );
     console.log(addressBook.addContact(contact1)); 
 
     const contact2 = new Contact(
-        "Rakesh", "Soni", "Bhopal", "India","In","1000000","9197979988","rakeshsoni395@gmail.com"
+        "Rakesh", "Soni", "Bhopal", "India","In","1000000","9197979988","rakesh395@gmail.com"
     )
     console.log(addressBook.addContact(contact2)); 
     console.log("All Contacts Before editing", addressBook.getAllContacts());
     console.log(addressBook.editContact("Rakesh", { address: "Pipariya", city: "New York", phone: "9234567890" }));
     console.log("All Contacts After editing", addressBook.getAllContacts());
+    console.log(addressBook.deleteContact("Rakesh"));
+    console.log("All Contacts After Deleting:", addressBook.getAllContacts());
 } catch (error) {
     console.error("Error:", error.message);
 }
